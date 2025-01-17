76d10dfe8042c8aaa06be52b81a7c32ca30b4bef
Reynaldo Gil <gilcu2mail.com>
2025-01-17 11:29:38 +0100

    working on authentication

--- src/BookTech/settings.py
+++ src/BookTech/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'library',
     "home",
     "movies",
+    "accounts",
 ]
 
 MIDDLEWARE = [
--- src/BookTech/urls.py
+++ src/BookTech/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('library/', include('library.urls')),
     path('', include('home.urls')),
     path('movies/', include('movies.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
